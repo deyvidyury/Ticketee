59407a396f9ce12f077cfc935cf9fabad32e90f7
Deyvid Yury <deyvidyury@yahoo.com.br>
2016-09-10 00:26:01 -0300

    Link tickets and users upon ticket creation

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -28,4 +28,6 @@ RSpec.feature "Users can create new projects" do
 		expect(page).to have_content "Project has not been created."
 		expect(page).to have_content "Name can't be blank"
 	end
+
+	
 end
